5acfdf72efb5880cdd8cad6a507e9d466600e5f5
lausandt <laurens.sandt@protonmail.com>
2023-06-07 11:02:56 +0200

    changed config file

--- calculator.py
+++ calculator.py
@@ -4,8 +4,10 @@ Calculator library containing basic math operations.
 """
 from typing import Union
 
-def add(first_term:Union[float,int], second_term:Union[float,int])->Union[float,int]:
+
+def add(first_term: Union[float, int], second_term: Union[float, int]) -> Union[float, int]:
     return first_term + second_term
 
-def subtract(first_term:Union[float,int], second_term;Union[float,int])->Union[float,int]:
+
+def subtract(first_term: Union[float, int], second_term: Union[float, int]) -> Union[float, int]:
     return first_term - second_term
