8bdb8bca78831aff7551d132ed9b5216f1679a7a
lausandt <laurens.sandt@protonmail.com>
2023-06-07 11:27:45 +0200

    took out typing

--- calculator.py
+++ calculator.py
@@ -2,16 +2,15 @@
 calculator.py
 Calculator library containing basic math operations.
 """
-from typing import Union
 
 
-def add(first_term: Union[float, int], second_term: Union[float, int]) -> Union[float, int]:
+def add(first_term, second_term):
     return first_term + second_term
 
 
-def subtract(first_term: Union[float, int], second_term: Union[float, int]) -> Union[float, int]:
+def subtract(first_term, second_term):
     return first_term - second_term
 
 
-def multiply(first_term: Union[float, int], second_term: Union[float, int]) -> Union[float, int]:
+def multiply(first_term, second_term):
     return first_term * second_term
